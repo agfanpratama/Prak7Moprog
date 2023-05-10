91c14e0d69450863a68fd7a966e428ddb4b14d88
Agfan Herru Pratama <107943090+agfanpratama@users.noreply.github.com>
2023-05-10 21:57:11 +0700

    Membuat Internal Storage pada Mobile Programming]

---
+++ InternalStorageDemo/InternalStorageDemo/app/src/main/java/com/example/internalstoragedemo/MainActivity2.java
@@ -0,0 +1,48 @@
+package com.example.internalstoragedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView getname, getpass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        getname = (TextView) findViewById(R.id.getname);
+        getpass = (TextView) findViewById(R.id.getpass);
+    }
+    public void load(View view)
+    {
+        try {
+            FileInputStream fileInputStream = openFileInput("Code.txt");
+            int read = -1;
+            StringBuffer buffer = new StringBuffer();
+            while((read = fileInputStream.read())!= -1){
+                buffer.append((char)read);
+            }
+            Log.d("Code", buffer.toString());
+            String name = buffer.substring(0,buffer.length());
+            String pass = buffer.substring(buffer.length());
+            getname.setText(name);
+            getpass.setText(pass);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(this, "Loaded", Toast.LENGTH_SHORT).show();
+    }
+    public void back( View view)
+    {
+        Toast.makeText(this, "Back", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
